1ccb4dc7eac12f407ac6284c21f8feb5c127c3f8
Leonardo Abdalla <leonardoabdalladeveloper@gmail.com>
2022-03-11 18:36:55 -0300

    Requisito 4

--- src/pages/Game/index.js
+++ src/pages/Game/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import { connect } from 'react-redux';
 import { fetchToken } from '../../services/Api';
+import Header from '../Header';
 
 class Game extends React.Component {
   componentDidMount = async () => {
@@ -10,7 +11,10 @@ class Game extends React.Component {
 
   render() {
     return (
-      <h1>TRIVIA</h1>
+      <div>
+        <h1>TRIVIA</h1>
+        <Header />
+      </div>
     );
   }
 }
---
+++ src/pages/Header/index.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { MD5 } from 'crypto-js';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Header extends Component {
+  getGravatar = () => {
+    const { email } = this.props;
+    const gravatarHash = MD5(email)
+      .toString()
+      .toLowerCase()
+      .trim();
+    const gravatarEmail = `https://www.gravatar.com/avatar/${gravatarHash}`;
+    return gravatarEmail;
+  }
+
+  render() {
+    const { name, score } = this.props;
+    return (
+      <header>
+        <img
+          data-testid="header-profile-picture"
+          src={ this.getGravatar() }
+          alt="Foto do Perfil"
+        />
+        <p data-testid="header-player-name">{ name }</p>
+        <p data-testid="header-score">{ score }</p>
+      </header>
+    );
+  }
+}
+const mapStateToProps = ({ loginReducer:
+  { gravatarEmail: { name, email }, score } }) => ({
+  name,
+  email,
+  score,
+});
+
+Header.propTypes = {
+  name: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Header);
