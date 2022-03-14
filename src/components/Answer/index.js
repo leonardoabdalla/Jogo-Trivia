f201d98ca391a087bdeee2a5db2d45dcf4468b25
Léia Ribeiro <leiaribeirot@gmail.com>
2022-03-14 04:01:22 -0400

    Requisitos 5 e 6 completos

--- src/components/Answer/index.js
+++ src/components/Answer/index.js
@@ -2,52 +2,43 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Answer extends Component {
-  answersQuestion = () => {
+  randomAnswers = () => {
     const { question } = this.props;
-    const correct = question.correct_answer;
-    const incorrects = question.incorrect_answers;
-
-    return [
-      <button
-        type="button"
-        data-testid="correct-answer"
-        id="correct-answer"
-        key="correct-answer"
-      >
-        {correct}
-      </button>,
-      incorrects.map((answer, index) => (
-        <button
-          type="button"
-          data-testid={ `wrong-answer-${index}` }
-          key={ index }
-        >
-          {answer}
-        </button>
-
-      )),
-    ];
+    const arrayAnswers = [question.correct_answer, ...question.incorrect_answers];
+    const randomAnswers = arrayAnswers.sort(() => Math.random() - Number('0.5'));
+    return randomAnswers;
   }
 
   render() {
     const { question } = this.props;
-    return (
-      <>
-        <div>
-          <div data-testid="answer-options" />
-          <h2 data-testid="question-category">{question.category}</h2>
-          <h3 data-testid="question-text">{question.question}</h3>
-          {this.answersQuestion()}
-          <div data-testid="answer-options" />
-        </div>
-        <br />
-        <button type="button" disabled>Próxima Pergunta</button>
 
-      </>
+    return (
+      <div>
+        <h2 data-testid="question-category">{ question.category }</h2>
+        <h3 data-testid="question-text">{ question.question }</h3>
+        <div data-testid="answer-options">
+          { this.randomAnswers(question).map((answer, index) => (
+            question.correct_answer === answer
+              ? (
+                <button
+                  type="button"
+                  data-testid="correct-answer"
+                >
+                  { answer }
+                </button>)
+              : (
+                <button
+                  type="button"
+                  data-testid={ `wrong-answer-${index}` }
+                >
+                  { answer }
+                </button>)
+          )) }
+        </div>
+      </div>
     );
   }
 }
-
 Answer.propTypes = {
   question: PropTypes.string.isRequired,
 };
